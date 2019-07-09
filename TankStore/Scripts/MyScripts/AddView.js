bcf30721ba8e14591da30ba169680822ba67d53a
Albert <albert51299@gmail.com>
2019-07-09 12:41:40 +0500

    Fix add view

---
+++ TankStore/Scripts/MyScripts/AddView.js
@@ -0,0 +1,25 @@
+﻿
+var forms = document.getElementsByTagName("form");
+forms[0].addEventListener("submit", SubmitHandler);
+
+function SubmitHandler(event) {
+    var name = document.getElementById("tankName");
+    var country = document.getElementById("tankCountry");
+    var cost = document.getElementById("tankCost");
+    var count = document.getElementById("tankCount");
+    if ((name.value == "") || (country.value == "") || (parseInt(cost.value) <= 0) || (parseInt(count.value) < 0)) {
+        alert("Incorrect data");
+        event.preventDefault();
+    }
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+var btnCancel = document.getElementById("btnCancel");
+btnCancel.addEventListener("click", CancelHandler);
+
+function CancelHandler(event) {
+    window.location.href = "/Admin/ControlPanel/";
+}
